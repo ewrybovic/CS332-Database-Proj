5f92c068768836096e46592b7c342721c075a8dd
ewrybovic <42895807+ewrybovic@users.noreply.github.com>
2018-11-15 11:52:49 -0800

    Added some comments

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <html>
 	<head>
+		<!-- Link to the CSS stylesheet -->
 		<link rel="stylesheet" href="css/styles.css"></link>
 	</head>
 	<title>Project for CS332</title>
 	<body>
 		<h1>Database Project for CS332</h1>
 		<div class="tab">
+			<!-- 	class for CSS styling     on click listener calls function from tab.js  -->
 			<button class="tablinks" onclick="openQuery(event, 'Professor')">Professor</button>
 			<button class="tablinks" onclick="openQuery(event, 'Student')">Student</button>
 		</div>
 		
 		<div id="Professor" class="tabcontent">
+			<!--  action goes to the same webpage so the table will be shown in the same
+				  page cause I think it looks better		   -->
 			<form action="index.php" target="_self" method="post">
 				<fieldset>
 					<legend>Professor SSN</legend>
 					SSN of Professor<br>
+					<!-- name is there so in PHP we can see what the value of it is -->
 					<input type="text" name="PSsn"><br><br>
 					<input type="submit" name="btnProfSsn" class="submitButton" value="Submit">
 				</fieldset>
@@ -30,6 +35,7 @@
 					<input type="submit" name="btnProfCourse" class="submitButton" value="Submit">
 				</fieldset>
 			</form>
+			<!-- Link the php script for Professor from a different file -->
 			<?php include 'php/Professor.php';?>
 		</div>
 		<div id="Student" class="tabcontent">
@@ -37,5 +43,6 @@
 			<?php include 'php/Student.php';?>
 		</div>
 	</body>
+		<!-- Link to the javascript file -->
 		<script src="js/tab.js"></script>
 </html>
