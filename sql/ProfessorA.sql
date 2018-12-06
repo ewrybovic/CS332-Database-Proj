e8b54a67dd21c7dcf91a915d6f044bf39cd9078f
ewrybovic <42895807+ewrybovic@users.noreply.github.com>
2018-12-06 15:07:11 -0800

    Update Professor Query

--- sql/ProfessorA.sql
+++ sql/ProfessorA.sql
@@ -1,3 +1,3 @@
-select P.P_Name, C.C_Title, P.P_SSN, CS.CS_Classroom, CS.CS_MeetingDays, CS.CS_BeginningTime, CS.CS_EndingTime
+select P.P_Name, C.C_Title, CS.CS_Classroom, CS.CS_MeetingDays, CS.CS_BeginningTime, CS.CS_EndingTime
 from Professor P, Course C, ClassSection CS
 where P.P_Ssn = REPLACE and P.P_Ssn = CS_PSsn and C.C_Num = CS.CS_CourseNumber;
